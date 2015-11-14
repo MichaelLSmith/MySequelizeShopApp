2bcc0a17011e1581c7af8e0cc5f792b8f9dd9e7b
MichaelLSmith <mlsmith45@gmail.com>
2015-11-14 17:51:42 -0500

    about to add login feature

--- api/routes/users.js
+++ api/routes/users.js
@@ -10,23 +10,23 @@ router.get('/',function(req,res){
 	});
 });
 
-router.get('/:userId',function(req,res){
-	models.Users.findAll({
-		where: {
-		    id: req.params.userId,
-		  }
-     }).then(function(user){
-     	res.json({
-     		user:user
-     	});
-     });
-});
+// router.get('/:userId',function(req,res){
+// 	models.Users.findAll({
+// 		where: {
+// 		    id: req.params.userId,
+// 		  }
+//      }).then(function(user){
+//      	res.json({
+//      		user:user
+//      	});
+//      });
+// });
 
 router.post('admin_login', function (req,res){
     console.log('POST /login request');
 
-    console.log('request: '+req);
-    console.log('response: '+res);
+    console.log('request: '+req.data);
+    console.log('response: '+res.data);
 
     //checks to see if user exists in db.
     //if user exists, it will send back an auth token.
@@ -36,13 +36,18 @@ router.post('admin_login', function (req,res){
     var adminCheck_obj = {}; 
 })
 
-router.post('/createDummyUser',function(req,res){
+router.get('/createDummyUser',function (req,res){
+    console.log('GET createDummyUser req: '+ req);
 	var user_obj = {
 		email:'r2@shop.ca',
-		password:'123'
+		password:'123',
+        authToken: '3k5k4098dKT'
 	}
 
-	models.Users.create(user_obj).then(function(users){
+    console.log('request: '+ req);
+    console.log('response: '+ res);
+
+	models.Users.create(user_obj).then(function (users){
 		res.json({
 			users:users
 		});
